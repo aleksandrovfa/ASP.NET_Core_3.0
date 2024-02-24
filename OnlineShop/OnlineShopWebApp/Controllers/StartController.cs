39ff7867be17f92b3d79073ab791a101443ec11e
Fedor Aleksandrov <aleksandrovfa@yandex.ru>
2024-02-24 17:09:46 +0300

    *

---
+++ OnlineShop/OnlineShopWebApp/Controllers/StartController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OnlineShopWebApp.Models;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class StartController : Controller
+    {
+        private readonly ILogger<StartController> _logger;
+        // branch 1
+        public StartController(ILogger<StartController> logger)
+        {
+            _logger = logger;
+        }
+
+        public string Hello()
+        {
+            var currentTime = DateTime.Now;
+            string greeting;
+
+            if (currentTime.Hour >= 0 && currentTime.Hour < 6)
+            {
+                greeting = "Доброй ночи";
+            }
+            else if (currentTime.Hour >= 6 && currentTime.Hour < 12)
+            {
+                greeting = "Доброе утро";
+            }
+            else if (currentTime.Hour >= 12 && currentTime.Hour < 18)
+            {
+                greeting = "Добрый день";
+            }
+            else
+            {
+                greeting = "Добрый вечер";
+            }
+
+            return greeting;
+        }
+
+      
+    }
+}
