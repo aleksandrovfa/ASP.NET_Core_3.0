dd7209166a1ae37eb2258b4602b8701c28432dba
Fedor Aleksandrov <aleksandrovfa@yandex.ru>
2024-02-24 17:28:25 +0300

    *

---
+++ OnlineShop/OnlineShopWebApp/Controllers/CalculatorController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OnlineShopWebApp.Models;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        private readonly ILogger<CalculatorController> _logger;
+        // branch 1
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        public string Index(string a = "0", string b = "0")
+        {
+            string result = (Convert.ToDouble(a) + Convert.ToDouble(b)).ToString();
+
+            return $"{a} + {b} = {result}";
+        }
+
+       
+    }
+}
