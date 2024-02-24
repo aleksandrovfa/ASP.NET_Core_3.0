e976b640cb6d69cf8675f492748bcfcbab514269
Fedor Aleksandrov <aleksandrovfa@yandex.ru>
2024-02-24 19:25:33 +0300

    *

---
+++ OnlineShop/OnlineShopWebApp/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OnlineShopWebApp.Models;
+
+namespace OnlineShopWebApp.Controllers
+{
+
+    public class ProductController : Controller
+    {
+        private readonly ProductRepository productRepository;
+
+        public ProductController()
+        {
+            productRepository = new ProductRepository();
+        }
+        public string Index(int id)
+        {
+            var product = productRepository.TryGetById(id);
+            if (product == null)
+            {
+                return "Продукта с таким id не существует";
+            }
+            return $"{product}\n{product.Description}";
+        }
+    }
+}
+
--- OnlineShop/OnlineShopWebApp/Models/Product.cs
+++ OnlineShop/OnlineShopWebApp/Models/Product.cs
@@ -2,28 +2,24 @@
 {
     public class Product
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public double Cost { get; set; }
-        public string Description { get; set; }
+        private static int constantCounter = 0;
+        public int Id { get; }
+        public string Name { get; }
+        public decimal Cost { get; }
+        public string Description { get; }
+
+        public Product(string name, decimal cost, string description)
+        {
+            Id = constantCounter;
+            Name = name;
+            Cost = cost;
+            Description = description;
+            constantCounter++;
+        }
 
         public override string ToString()
         {
-            string result = string.Empty;
-            result += Id.ToString() + "\n";
-            result += Name.ToString() + "\n";
-            result += Cost.ToString();
-            return result;
-        }
-
-        public string ToString2()
-        {
-            string result = string.Empty;
-            result += Id.ToString() + "\n";
-            result += Name.ToString() + "\n";
-            result += Cost.ToString() + "\n";
-            result += Description.ToString();
-            return result;
+            return $"{Id}\n{Name}\n{Cost}";
         }
     }
 }
